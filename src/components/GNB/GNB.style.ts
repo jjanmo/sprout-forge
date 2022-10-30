73883da1b9697e6df3bee2955e71ff38195a4a99
jjanmo <jjanmo07@hanmail.net>
2022-10-30 18:04:03 +0900

    feat: add gnb component

---
+++ src/components/GNB/GNB.style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  width: 100%;
+  height: 6rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Logo = styled.div``;
+
+export const Info = styled.div``;
---
+++ src/components/GNB/GNB.tsx
@@ -0,0 +1,12 @@
+import * as S from './GNB.style';
+
+export default function GNB() {
+  return (
+    <S.Container>
+      <S.Logo>로고</S.Logo>
+      <S.Info>
+        <div>유저 이름</div>
+      </S.Info>
+    </S.Container>
+  );
+}
