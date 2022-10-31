16573c80201ea06bc127336bf5f35c6950c1c222
jjanmo <jjanmo07@hanmail.net>
2022-10-31 23:46:02 +0900

    feat: configure pwa configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
 
 const nextConfig = {
   reactStrictMode: true,
@@ -9,4 +11,14 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([
+  [
+    withPWA,
+    {
+      pwa: {
+        dest: 'public',
+      },
+    },
+  ],
+  nextConfig,
+]);
